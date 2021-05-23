cd252b799d6c7b7c078e62bcc8f699d9796610d5
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2021-05-23 05:52:21 +0200

    Building the structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ShoppingListComponent } from './components/shopping/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './components/shopping/shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { IngredientComponent } from './components/shopping/ingredient/ingredient.component';
 import { RecipeListComponent } from './components/recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './components/recipes/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe-detail.component';
 import { RecipeComponent } from './components/recipes/recipe/recipe.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
-import { ShoppingComponent } from './components/shopping/shopping.component';
 
 @NgModule({
   declarations: [
@@ -20,13 +18,11 @@ import { ShoppingComponent } from './components/shopping/shopping.component';
     HeaderComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
-    IngredientComponent,
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
     RecipeComponent,
     RecipesComponent,
-    ShoppingComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/recipes/recipe/recipe.component.ts
+++ src/app/components/recipes/recipe/recipe.component.ts
@@ -8,6 +8,10 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('A test name', 'a test description', 'a test image')
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
